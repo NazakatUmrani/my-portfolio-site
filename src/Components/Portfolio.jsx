6c33d4a7cc3169a1920d4bfd1feeef5f9e16c9c5
Nazakat Umrani <111883142+NazakatUmrani@users.noreply.github.com>
2023-09-16 07:48:18 +0000

    Customized my website

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/portfolio.jpeg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "blue background";
 
 /**
  * Project list
@@ -31,10 +31,10 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Number System Converter",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "It converts Numbers to another number systems",
+    url: "github.com/nazakatumrani/number-system-converter",
   },
   {
     title: "Web Development for Beginners",
@@ -47,6 +47,12 @@ const projectList = [
     description:
       "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
     url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+  },
+  {
+    title: "GitHub Codespaces and github.dev",
+    description:
+      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
+    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
   },
   {
     title: "GitHub Codespaces and github.dev",
